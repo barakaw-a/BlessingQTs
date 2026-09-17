8ab13854ebaeb2f102d3582a823547b9621506a9
Baraka Waweru <xbarakawaweru@gmail.com>
2026-09-17 12:55:31 -0400

    feat: updated simple_ma to return a series of signals based on moving average crossover

--- src/signaler.py
+++ src/signaler.py
@@ -6,28 +6,36 @@ import data_loader
 class Strategy:
     def __init__(self, db_path: str = 'market_data.db'):
         self.db_path = db_path
-        
-    def simple_ma(self, symbol : str, short_term = 50, long_term = 200) -> pd.Series:
+    
+    # Implemented a simple moving average with a short-term of 50 days and long-term of 200 days
+    # Returns a Series of orders based on crossovers
+    def moving_average(self, symbol : str, short_term = 50, long_term = 200):
         with sqlite3.connect(self.db_path) as con:
+             
+            date_price = con.execute("""
+                                        SELECT close, date
+                                        FROM prices
+                                        WHERE symbol = ?
+                                        ORDER BY DATE ASC
+                                    """, [symbol]).fetchall()
+                        
+            df_date_price = pd.DataFrame(date_price)
+            df_date_price.rename(columns = {0: 'price', 1: 'date'}, inplace = True)
             
-            short = con.execute("""    
-                                SELECT close
-                                FROM prices
-                                WHERE symbol = ?
-                                ORDER BY date DESC
-                                LIMIT ?    
-                                """, [symbol, short_term])
+            df_date_price['date'] = pd.to_datetime(df_date_price['date'])
+            df_date_price.set_index('date', inplace = True)
             
-            short_list = pd.DataFrame(short.fetchall())
-            short_sma = ((short_list.sum()[0])/short_term)
+            df_date_price['short_sma'] = (df_date_price['price'].rolling(window = short_term).mean())
+            df_date_price['long_sma'] = (df_date_price['price'].rolling(window = long_term).mean())
             
-            long = con.execute("""
-                                SELECT close
-                                FROM prices
-                                WHERE symbol = ?
-                                ORDER BY date DESC
-                                LIMIT ?
-                                """, [symbol, long_term])
-            long_list = pd.DataFrame(long.fetchall())
-            long_sma = ((long_list.sum()[0])/long_term)
+            df_date_price['signals'] = pd.Series(pd.NA, index = df_date_price.index, dtype = 'string')
             
+            valid = df_date_price['long_sma'].notna()
+            
+            df_date_price.loc[valid, 'signals'] = np.where(df_date_price.loc[valid, 'short_sma'] > df_date_price.loc[valid, 'long_sma'], 'Buy', 'Hold')
+            
+            return(df_date_price)
+            
+test = Strategy()
+
+print(test.moving_average("AAPL"))
